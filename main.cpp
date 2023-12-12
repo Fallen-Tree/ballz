7ba4f8903eb3a438d08a5665f3a90366d8911302
RomchikkF <r.fesenko@g.nsu.ru>
2023-12-12 22:08:49 +0700

    holes and score

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ const char *vertexShaderSource = "/vertex/standart.vshader";
 const char *fragmentShaderSource = "/fragment/standart.fshader";
 
 const char *cubeSource = "/cube.obj";
+
 class MovingBall : public Object {
  private:
     float MIN_VELOCITY = 0.001;
 
  public:
+    bool active = true;
     Vec3 velocity;
     float bounciness = 1;
     float friction = 4;
@@ -57,6 +59,30 @@ class MovingBall : public Object {
         }};
 
         transform = new Transform(position, Vec3(radius), Mat4(1.0));
+    }
+};
+
+class Hole : public Object {
+ private:
+    Vec3 NOWHERE = Vec3(0, 10000, 0);
+ public:
+    Hole(Vec3 position, float r) {
+        this->collider = new Collider{Sphere{
+            Vec3(0.0),
+            r,
+        }};
+
+        transform = new Transform(position, Vec3(r), Mat4(1.0));
+    }
+
+    void Consume(MovingBall *ball) {
+        ball->active = false;
+        ball->velocity = Vec3(0, 0, 0);
+        ball->transform->SetTranslation(transform->GetTranslation());
+        ball->animation = new Animation();
+        auto newTrans = *ball->transform;
+        newTrans.Translate(Vec3(0, -2, 0));
+        ball->animation->addAnimation(newTrans, 2.f);
     }
 };
 
@@ -131,8 +157,18 @@ class Cue : public Object {
 
 
 class GameManager : public Object {
+ private:
+    int score = 0;
+    void ShowScore() {
+        char buf[15];
+        snprintf(buf, sizeof(buf), "Score: %d", score);
+        this->text->SetContent(buf);
+    }
+
  public:
     std::vector<MovingBall*> balls;
+    std::vector<Hole*> holes;
+
     float minX = -10, maxX = 10, minZ = -10, maxZ = 10;
 
     void SetBorders(float minX, float maxX, float minZ, float maxZ) {
@@ -145,8 +181,17 @@ class GameManager : public Object {
     void Update(float dt) override {
         for (int i = 0; i < balls.size(); ++i) {
             auto ball = balls[i];
+            if (!ball->active) continue;
             Vec3 pos = ball->transform->GetTranslation();
             float r = ball->transform->GetScale().x;
+            for (auto h : holes) {
+                if (ball->collider->Collide(*ball->transform, h->collider, *h->transform)) {
+                    h->Consume(ball);
+                    score += 100;
+                    break;
+                }
+            }
+            if (!ball->active) continue;
             if (pos.x - r < minX) {
                 ball->transform->Translate((1 + ball->bounciness) * Vec3(minX - pos.x + r, 0, 0));
                 ball->velocity = Vec3(- ball->velocity.x, 0, ball->velocity.z) * ball->bounciness;
@@ -165,6 +210,7 @@ class GameManager : public Object {
             }
             for (int j = i + 1; j < balls.size(); ++j) {
                 auto ball2 = balls[j];
+                if (!ball2->active) continue;
                 if (ball->collider->Collide(*ball->transform, ball2->collider, *ball2->transform)) {
                     // collision
                     Vec3 pos2 = ball2->transform->GetTranslation();
@@ -181,9 +227,12 @@ class GameManager : public Object {
                 }
             }
         }
+        ShowScore();
     }
-    explicit GameManager(std::vector<MovingBall*> balls) {
+
+    explicit GameManager(std::vector<MovingBall*> balls, std::vector<Hole*> holes) {
         this->balls = balls;
+        this->holes = holes;
     }
 };
 
@@ -234,13 +283,33 @@ int main() {
     std::vector<MovingBall*> balls;
     int ballsCount = 5;
     float ballRadius = 0.3f;
+    float Y = -8;
 
     for (int i = 0; i < ballsCount; ++i) {
-        MovingBall *sphere = newBall(Vec3(i * 2 - 5, -8, -i), Vec3(3, 0, 1 + i), ballRadius,
+        MovingBall *sphere = newBall(Vec3(i * 2 - 5, Y, -i), Vec3(3, 0, 1 + i), ballRadius,
          shaderProgram, "/152.png", "/Cat_specular.png");
         balls.push_back(sphere);
         engine.AddObject<>(sphere);
     }
+
+    std::vector<Hole*> holes;
+    float holeRadius = 0.2f;
+    float borderX = 7.17f;
+    float borderZ = 3.59f;
+    float holeX = borderX - 0.25f;
+    float holeZ = borderZ - 0.25f;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            Hole *newHole = new Hole(Vec3(-holeX + 2 * j * holeX, Y, -holeZ + i * 2 * holeZ), holeRadius);
+            holes.push_back(newHole);
+        }
+        Hole *newHole = new Hole(Vec3(0, Y, -borderZ + i * 2 * borderZ), holeRadius);
+        holes.push_back(newHole);
+    }
+
+    GameManager *gameManager = new GameManager(balls, holes);
+    gameManager->SetBorders(-borderX, borderX, -borderZ, borderZ);
+
     Cue *cue = new Cue(balls, engine.camera, shaderProgram);
     engine.AddObject(cue);
 
@@ -256,9 +325,7 @@ int main() {
     pointer->text = new Text(textOcraBig, "+", SCR_WIDTH/2.f - 10, SCR_HEIGHT/2.f - 10, 1.f, Vec3(0, 0, 0));
     engine.AddObject<>(pointer);
 
-    GameManager *gameManager = new GameManager(balls);
-    gameManager->SetBorders(-7.2, 7.2, -3.6, 3.6);
-
+    gameManager->text = new Text(textOcra, "", 10.0f, 575.f, 1.f, Vec3(0, 0, 0));
     engine.AddObject(gameManager);
 
     Vec3 tablePosition = Vec3(0, -15.04, 0);
